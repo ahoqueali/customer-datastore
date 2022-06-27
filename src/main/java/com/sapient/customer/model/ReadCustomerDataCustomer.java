6560514f0ff6299bc0bca67a7a45119afe3dca57
Abdul Hoque Ali <abdali@WKMZT2E5494D.global.publicisgroupe.net>
2022-06-27 21:46:45 +0100

    Add model classes

---
+++ src/main/java/com/sapient/customer/model/ReadCustomerDataCustomer.java
@@ -0,0 +1,81 @@
+package com.sapient.customer.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.UUID;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * ReadCustomerDataCustomer
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-06-27T20:27:40.252Z[GMT]")
+
+
+public class ReadCustomerDataCustomer   {
+  @JsonProperty("CustomerId")
+  private UUID customerId = null;
+
+  public ReadCustomerDataCustomer customerId(UUID customerId) {
+    this.customerId = customerId;
+    return this;
+  }
+
+  /**
+   * Get customerId
+   * @return customerId
+   **/
+  @Schema(description = "")
+  
+    @Valid
+  @Size(min=36,max=36)   public UUID getCustomerId() {
+    return customerId;
+  }
+
+  public void setCustomerId(UUID customerId) {
+    this.customerId = customerId;
+  }
+
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ReadCustomerDataCustomer readCustomerDataCustomer = (ReadCustomerDataCustomer) o;
+    return Objects.equals(this.customerId, readCustomerDataCustomer.customerId);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(customerId);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ReadCustomerDataCustomer {\n");
+    
+    sb.append("    customerId: ").append(toIndentedString(customerId)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
